2ea0664de95bbe2fa345f8e5a7abc17f510d457c
kevintram <thekevintram@gmail.com>
2021-08-16 10:26:55 -0400

    Adds a switch

--- app/src/main/java/com/kiwicorp/timereminder/MainActivity.kt
+++ app/src/main/java/com/kiwicorp/timereminder/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kiwicorp.timereminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch: SwitchMaterial = findViewById(R.id._switch)
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Log.d("MainActivity", "YOOOOOOOO")
+            }
+        }
     }
 }
